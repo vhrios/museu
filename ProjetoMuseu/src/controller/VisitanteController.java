5de5cf30db8354761ad22e1cacc561a6d668ff28
Ericka <Ericka@10.68.77.51>
2015-11-17 16:31:21 -0200

    Controller Ericka

--- ProjetoMuseu/src/controller/VisitanteController.java
+++ ProjetoMuseu/src/controller/VisitanteController.java
@@ -9,20 +9,18 @@ import persistence.IVisitante;
 import persistence.VisitanteDAO;
 
 public class VisitanteController {
-
-	public void salvaVisitante(Visitante v) {
+	
+//	private List<Exposicao> lista = null;
+	
+	public void salvaVisitante(Visitante v){
 		IVisitante iv = new VisitanteDAO();
 		try {
-			if (iv.manter(v))
-				JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso", "Sucesso",
-						JOptionPane.INFORMATION_MESSAGE);
-			else
-				JOptionPane.showMessageDialog(null, "Cadastrado NÃO EFETUADO", "Falha",
-						JOptionPane.INFORMATION_MESSAGE);
+			iv.insereVisitante(v);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO",
+					JOptionPane.ERROR_MESSAGE);
 		}
-
+		
 	}
-
+	
 }
